fc76a559e61fedb4edc33c536386d9188a48c739
eudeseadsenac-ui <eudeseadsenac@gmail.com>
2026-08-30 01:10:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -343,6 +343,9 @@ def enviar_rich_message(chat_id):
     def enviar_menu_editora(chat_id, editora):
     url = f"{SUPABASE_URL}/quadrinhos_supercolecoes"
 
+def enviar_menu_editora(chat_id, editora):
+    url = f"{SUPABASE_URL}/quadrinhos_supercolecoes"
+
     params = {
         "editora": f"eq.{editora}",
         "select": "personagem"
